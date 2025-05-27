cbc46d6e6341134da597fa74f374657aa40e2cf0
Srleo12 <carlosnatha2345@gmail.com>
2025-05-27 13:17:46 -0300

    .

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ LabelBase.register(
     fn_regular="fonts/Roboto-Regular.ttf",
     fn_bold="fonts/Roboto-Bold.ttf"
 )
-
 # Configuração padrão para todos os widgets
 from kivy.config import Config
 Config.set('kivy', 'default_font', ['Roboto', 'Arial'])
+
 
 class DarkTextInput(TextInput):
     def __init__(self, **kwargs):
@@ -42,12 +42,14 @@ class DarkButton(Button):
         self.color = (1, 1, 1, 1)  # texto branco
         self.size_hint_y = None
         self.height = 40
+
 class FormularioOS(BoxLayout):
     def limpar_campos(self):
         self.ids.equipamento.text = ""
         self.ids.problema.text = ""
         self.ids.solucao.text = ""
-        self.ids.tempo_reparo.text = ""
+        self.ids.iniciar_os.text = ""
+        self.ids.fim_os.text = ""
 
     def validar_campos(self):
         if not self.ids.equipamento.text.strip():
@@ -58,9 +60,6 @@ class FormularioOS(BoxLayout):
             return False
         if not self.ids.solucao.text.strip():
             self.mostrar_mensagem("Descreva a solução!")
-            return False
-        if not self.ids.tempo_reparo.text.strip():
-            self.mostrar_mensagem("Informe o tempo de reparo!")
             return False
         if not self.ids.inicio_os.text.strip():
             self.mostrar_mensagem("Informe o início da OS!")
@@ -81,25 +80,19 @@ class FormularioOS(BoxLayout):
             self.mostrar_mensagem("O fim da OS deve ser depois do início!")
             return False
 
-        try:
-            float(self.ids.tempo_reparo.text)
-        except ValueError:
-            self.mostrar_mensagem("Tempo de reparo deve ser um número!")
-            return False
-        return True
-
     def enviar_os(self):
         if not self.validar_campos():
             return
         
+
         data = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         equipamento = self.ids.equipamento.text
         problema = self.ids.problema.text
         solucao = self.ids.solucao.text
         inicio = datetime.strptime(self.ids.inicio_os.text.strip(), "%d/%m/%Y %H:%M")
         fim = datetime.strptime(self.ids.fim_os.text.strip(), "%d/%m/%Y %H:%M")
-        tempo_reparo = (fim - inicio).total_seconds() / 60  # em minutos
-        
+        tempo_reparo = (fim - inicio).total_seconds() / 60  # em minutos     
+
         adicionar_os(data, equipamento, problema, solucao, tempo_reparo, inicio, fim)
         self.mostrar_mensagem("OS salva com sucesso!")
         self.limpar_campos()
@@ -111,9 +104,9 @@ class FormularioOS(BoxLayout):
         scroll = ScrollView()
         layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
         layout.bind(minimum_height=layout.setter('height'))
-        
+
         for os in dados:
-            texto =     f"Início: {os[6]}\n"f"Fim: {os[7]}\n"f"Equipamento: {os[2]}\n"f"Problema: {os[3]}\n"f"Solução: {os[4]}\n"f"Tempo: {os[5]:.0f} minutos"
+            texto = f"Início: {os[6]}\n"f"Fim: {os[7]}\n"f"Equipamento: {os[2]}\n"f"Problema: {os[3]}\n"f"Solução: {os[4]}\n"f"Tempo: {os[5]:.0f} minutos"
             label = Label(text=texto, size_hint_y=None, height=150, halign="left", valign="top")
             layout.add_widget(label)
         
